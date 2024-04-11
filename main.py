136b4114a907d5257a28b1d1581946bb6d0e7f09
Nick <filatovnick11@gmail.com>
2024-04-11 20:57:54 +0100

    movement, animation, lie down

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import arcade
 from constants import *
+from nick import *
 
 
 class Game(arcade.Window):
@@ -10,22 +11,48 @@ class Game(arcade.Window):
         for i in range(1, 16):
             self.background_textures.append(arcade.load_texture(f'background/Map{i}.png'))
         self.index_texture = 0
+        self.game = True
+        self.nick = Nick()
+        self.is_walk = False
+        self.lines = arcade.SpriteList()
+
 
     def setup(self):
         pass
 
     def update(self, delta_time: float):
-        pass
+        if self.game:
+            self.nick.update()
+            if self.is_walk:
+                self.nick.update_animation(delta_time)
 
     def on_draw(self):
         self.clear((255, 255, 255))
         arcade.draw_texture_rectangle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT, self.background_textures[self.index_texture])
+        self.nick.draw()
+        self.lines.draw()
 
     def on_key_press(self, symbol: int, modifiers: int):
-        pass
+        if symbol == arcade.key.LEFT:
+            self.is_walk = True
+            self.nick.side = 'left'
+            self.nick.set_side()
+            self.nick.change_x = -PLAYER_MOVEMENT_SPEED
+
+        if symbol == arcade.key.RIGHT:
+            self.is_walk = True
+            self.nick.side = 'right'
+            self.nick.set_side()
+            self.nick.change_x = PLAYER_MOVEMENT_SPEED
+
+        if symbol == arcade.key.DOWN:
+            self.nick.to_down()
 
     def on_key_release(self, symbol: int, modifiers: int):
-        pass
+        if symbol == arcade.key.LEFT or symbol == arcade.key.RIGHT or symbol == arcade.key.DOWN:
+            self.nick.change_x = 0
+            self.is_walk = False
+            self.nick.set_texture(0)
 
 
 game = Game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
--- nick.py
+++ nick.py
@@ -8,5 +8,27 @@ class Nick(animate.Animate):
         super().__init__('go_bill/0.gif', SCALING)
         self.center_x = 100
         self.center_y = 100
+        self.left_textures = []
+        self.right_textures = []
+        self.side = 'right'
+        self.right_down = arcade.load_texture('bill_textures/BillLayingDown.png', flipped_horizontally=False)
+        self.left_down = arcade.load_texture('bill_textures/BillLayingDown.png', flipped_horizontally=True)
         for i in range(6):
-            self.append_texture(arcade.load_texture(f'go_bill/{i}.gif'))
+            self.left_textures.append(arcade.load_texture(f'go_bill/{i}.gif', flipped_horizontally=True))
+            self.right_textures.append(arcade.load_texture(f'go_bill/{i}.gif', flipped_horizontally=False))
+
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+    def set_side(self):
+        if self.side == 'left':
+            self.textures = self.left_textures
+        elif self.side == 'right':
+            self.textures = self.right_textures
+
+    def to_down(self):
+        if self.side == 'right':
+            self.texture = self.right_down
+        elif self.side == 'left':
+            self.texture = self.left_down
